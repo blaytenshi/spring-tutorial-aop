f22928926c7477dfc5a7b7d9b2e7150078749ff1
Jimmy Tang <jimmyguojing.tang@uts.edu.au>
2016-03-02 17:18:42 +1100

    Udemy Spring Tutorial 79 - Pointcut Wildcards

--- src/com/caveofprogramming/spring/aop/Camera.java
+++ src/com/caveofprogramming/spring/aop/Camera.java
@@ -8,4 +8,18 @@ public class Camera {
 	public void snap() {
 		System.out.println("snap!");
 	}
+	
+	public void snap(int exposure) {
+		System.out.println("snap! Exposure: " + exposure);
+	}
+	
+	public String snap(String name) {
+		System.out.println("snap! Name: " + name);
+		
+		return name;
+	}
+	
+	public void snapNightTime() {
+		System.out.println("Snap night mode...");
+	}
 }
--- src/com/caveofprogramming/spring/aop/Logger.java
+++ src/com/caveofprogramming/spring/aop/Logger.java
@@ -11,8 +11,21 @@ import org.springframework.stereotype.Component;
 public class Logger {
 	
 	// This is now a reusable aspect method! :D
-	@Pointcut("execution(void com.caveofprogramming.spring.aop.Camera.snap())")
+	// The .. means takes any arguments! The .. is a wildcard.
+	@Pointcut("execution(* com.caveofprogramming.spring.aop.Camera.*(..))")
 	public void cameraSnap() {
+		
+	}
+	
+	// Pointcut will execute with any return type
+	@Pointcut("execution(* com.caveofprogramming.spring.aop.Camera.snap(String))")
+	public void cameraSnapName() {
+		
+	}
+	
+	// Pointcut will execute with any return type of any class in any package with any method and any argument
+	@Pointcut("execution(* *.*(..))")
+	public void cameraRelatedAction() {
 		
 	}
 	
@@ -20,5 +33,15 @@ public class Logger {
 	// Advice
 	public void aboutToTakePhoto() {
 		System.out.println("About to take Photo...");
+	}
+	
+	@Before("cameraSnapName()")
+	public void aboutToTakePhotoWithName() {
+		System.out.println("About to take photo with name...");
+	}
+	
+	@Before("cameraRelatedAction()")
+	public void aboutToDoCameraRelatedAction() {
+		System.out.println("Doing something related to cameras...");
 	}
 }
