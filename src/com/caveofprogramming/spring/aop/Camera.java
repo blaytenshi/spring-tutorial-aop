13c89d37672ef5afa9a29153076b0a8534292d00
Jimmy Tang <jimmyguojing.tang@uts.edu.au>
2016-03-02 17:00:16 +1100

    Udemy Spring Tutorial 78 - Annotation Based Aspects

--- src/com/caveofprogramming/spring/aop/Camera.java
+++ src/com/caveofprogramming/spring/aop/Camera.java
@@ -1,5 +1,8 @@
 package com.caveofprogramming.spring.aop;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Camera {
 	// point cut defines where a join point is
 	public void snap() {
--- src/com/caveofprogramming/spring/aop/Logger.java
+++ src/com/caveofprogramming/spring/aop/Logger.java
@@ -1,9 +1,22 @@
 package com.caveofprogramming.spring.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
+@Aspect
+@Component
 // Aspect
 public class Logger {
 	
+	// This is now a reusable aspect method! :D
+	@Pointcut("execution(void com.caveofprogramming.spring.aop.Camera.snap())")
+	public void cameraSnap() {
+		
+	}
+	
+	@Before("cameraSnap()")
 	// Advice
 	public void aboutToTakePhoto() {
 		System.out.println("About to take Photo...");
