dc9867db23f1483007ba2ba6dcb715f576ed6ed7
Jimmy Tang <blay.tenshi@gmail.com>
2016-03-11 23:41:57 +1100

    Udemy Spring Tutorial 89 - Combining Pointcuts

--- src/com/caveofprogramming/spring/aop/Logger.java
+++ src/com/caveofprogramming/spring/aop/Logger.java
@@ -1,5 +1,6 @@
 package com.caveofprogramming.spring.aop;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -10,20 +11,26 @@ import org.springframework.stereotype.Component;
 // Aspect
 public class Logger {
 	
-	// We've chosen the names to match what they are with the method that is being called but this is purely for consistencies sake with the method they're being called from. They don't have to match the names of the arguments.
 	@Pointcut("args(exposure, aperture)")
-	// this is where we're telling them what types they are...
 	public void somePointcut(int exposure, double aperture) {
 		
 	}
 	
-	// We now need to match the names we declared in the @PointCut annotation
-	@Before("somePointcut(exposure, aperture)")
+	@Pointcut("target(com.caveofprogramming.spring.aop.Camera)")
+	public void targetCamera() {
+		
+	}
+	
+	@Before("targetCamera() && somePointcut(exposure, aperture)")
 	// Advice
-	// The arguments names that you supply in the advice arguments need to match what's in the @Before annotation but they don't need to be in order. You don't even need the JoinPoint object anymore.
 	public void somePointcutDemo(double aperture, int exposure) {
 		System.out.println("********** Before demo **********");
 		System.out.printf("exposure %d, aperture %.2f\n", exposure, aperture);
+	}
+	
+	@After("within(com.caveofprogramming.spring.aop.*) && !@annotation(Deprecated)")
+	public void someAfterAdvice() {
+		System.out.println("After Advice Running!!!!!!!!!!!!!!!!!!!");
 	}
 
 }
